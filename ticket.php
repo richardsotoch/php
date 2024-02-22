829871ebcac051f7b46f9d715821bde8f6c5a18a
richardsotoch <160868214+richardsotoch@users.noreply.github.com>
2024-02-22 12:37:17 -0500

    Update ticket.php

--- ticket.php
+++ ticket.php
@@ -132,7 +132,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["generarTicket"])) {
     </div>
 
     <script>
-        // Tu código JavaScript existente aquí
+        
     </script>
 
 </body>
